9dea8a20184dbe625e7f70dbd622c5874140d4a3
tazrian-amin <amin.tazrian1979@gmail.com>
2022-10-27 07:40:18 +0600

    final commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.use(cors());
 
@@ -12,6 +13,27 @@ app.get('/', (req, res) => {
 
 app.get('/news-categories', (req, res) => {
     res.send(categories);
+});
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news);
+    }
+    else {
+        const categoryNews = news.filter(n => n.category_id === id);
+        res.send(categoryNews);
+    }
+});
+
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
 })
 
 app.listen(port, () => {
